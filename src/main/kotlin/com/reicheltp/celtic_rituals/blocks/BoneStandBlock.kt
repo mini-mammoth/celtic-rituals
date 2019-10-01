00af1e6aa9bb439dc77af38bc274aae44150b0c2
Raphael Zöllner <rz-funs@web.de>
2019-10-01 23:12:27 +0200

    added direction to BoneStandBlock placement

--- src/main/kotlin/com/reicheltp/celtic_rituals/blocks/BoneStandBlock.kt
+++ src/main/kotlin/com/reicheltp/celtic_rituals/blocks/BoneStandBlock.kt
@@ -5,12 +5,24 @@ import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.material.Material
 import net.minecraft.block.material.MaterialColor
+import net.minecraft.entity.LivingEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.state.StateContainer
+import net.minecraft.state.properties.BlockStateProperties
 import net.minecraft.util.BlockRenderLayer
+import net.minecraft.util.Direction
 import net.minecraft.util.ResourceLocation
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 class BoneStandBlock() : Block(Properties.create(Material.ROCK, MaterialColor.SAND).hardnessAndResistance(2.0f)) {
     init {
         registryName = ResourceLocation(MOD_ID, "bone_stand")
+    }
+
+    override fun fillStateContainer(builder: StateContainer.Builder<Block, BlockState>) {
+        builder.add(BlockStateProperties.FACING)
+        super.fillStateContainer(builder)
     }
 
     override fun getRenderLayer(): BlockRenderLayer {
--- src/main/kotlin/com/reicheltp/celtic_rituals/events/PlayerEvents.kt
+++ src/main/kotlin/com/reicheltp/celtic_rituals/events/PlayerEvents.kt
@@ -11,7 +11,10 @@ import net.minecraft.block.Blocks
 import net.minecraft.block.FarmlandBlock
 import net.minecraft.block.GrassBlock
 import net.minecraft.block.SnowyDirtBlock
+import net.minecraft.entity.LivingEntity
 import net.minecraft.item.Items
+import net.minecraft.state.properties.BlockStateProperties
+import net.minecraft.util.Direction
 import net.minecraft.util.Hand
 import net.minecraft.util.SoundCategory
 import net.minecraft.util.SoundEvents
@@ -34,8 +37,22 @@ object PlayerEvents {
         Items.STONE_SHOVEL
         if (heldItemStackInMainHand != null && heldItemStackInMainHand.item == Items.BONE && block is GrassBlock || block == Blocks.DIRT)
         {
-            event.world.setBlockState(bonePos, ModBlocks.BONE_STAND?.defaultState)
+            val boneStandState = ModBlocks.BONE_STAND?.defaultState;
+
+            event.world.setBlockState(bonePos, boneStandState?.with(BlockStateProperties.FACING, getFacingFromEntity(bonePos, event.player)))
             event.world.playSound(event.player, event.pos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0f, 1.0f)
         }
+    }
+
+    private fun getFacingFromEntity(pos: BlockPos, entity: LivingEntity) : Direction
+    {
+        val direction = Direction.getFacingFromVector(entity.posX - pos.x, 0.0, entity.posZ - pos.z)
+
+        System.out.println(direction)
+
+        if (direction != Direction.DOWN && direction != Direction.UP)
+            return direction
+        else
+            return Direction.WEST
     }
 }
