5a4c8b5409f4d051196c5a1712ba51847255a3c9
Raphael Zöllner <rz-funs@web.de>
2019-10-03 00:58:35 +0200

    Add default numberOfEntities param to spawnEntityAroundPosition

--- src/main/kotlin/com/reicheltp/celtic_rituals/utils/Spawn.kt
+++ src/main/kotlin/com/reicheltp/celtic_rituals/utils/Spawn.kt
@@ -12,42 +12,46 @@ import java.util.Random
 
 object Spawn {
     // spawns the specified entity randomly around the blockPos
-    fun <T : Entity> spawnEntityAroundPosition(world: World, entityType: EntityType<T>, playerIn: PlayerEntity, blockPos: BlockPos, minDistance: Int, maxDistance: Int) : Boolean
+    fun <T : Entity> spawnEntityAroundPosition(world: World, entityType: EntityType<T>, playerIn: PlayerEntity, blockPos: BlockPos, minDistance: Int, maxDistance: Int, numberOfEntites: Int = 1) : Boolean
     {
         require(minDistance < maxDistance) { "maxDistance must be greater than minDistance" }
-
+        require(numberOfEntites > 0) { "numberOfEntites must be greater than 0" }
         val r = Random()
 
-        val xDist = r.nextInt((maxDistance - minDistance) + 1) + minDistance
-        val xOffset = if (r.nextBoolean()) xDist else (-1) * xDist
-
-        val zDist = r.nextInt((maxDistance - minDistance) + 1) + minDistance
-        val zOffset = if (r.nextBoolean()) zDist else (-1) * zDist
-
-        var mobPos = BlockPos(blockPos.x + xOffset, blockPos.y, blockPos.z + zOffset)
-
-        val blockState = world.getBlockState(mobPos)
-
-        //update y position such that mob spawns on the ground
-        if (blockState.getCollisionShape(world, mobPos).isEmpty())
+        var success = true
+        for (i in 1..numberOfEntites)
         {
-            var updatedBlockPos = mobPos.offset(Direction.DOWN)
-            while (world.getBlockState(updatedBlockPos).getCollisionShape(world, updatedBlockPos).isEmpty)
+            val xDist = r.nextInt((maxDistance - minDistance) + 1) + minDistance
+            val xOffset = if (r.nextBoolean()) xDist else (-1) * xDist
+
+            val zDist = r.nextInt((maxDistance - minDistance) + 1) + minDistance
+            val zOffset = if (r.nextBoolean()) zDist else (-1) * zDist
+
+            var mobPos = BlockPos(blockPos.x + xOffset, blockPos.y, blockPos.z + zOffset)
+
+            val blockState = world.getBlockState(mobPos)
+
+            //update y position such that mob spawns on the ground
+            if (blockState.getCollisionShape(world, mobPos).isEmpty())
             {
-                mobPos = updatedBlockPos
-                updatedBlockPos = mobPos.offset(Direction.DOWN)
+                var updatedBlockPos = mobPos.offset(Direction.DOWN)
+                while (world.getBlockState(updatedBlockPos).getCollisionShape(world, updatedBlockPos).isEmpty)
+                {
+                    mobPos = updatedBlockPos
+                    updatedBlockPos = mobPos.offset(Direction.DOWN)
+                }
             }
-        }
-        else
-        {
-            var updatedBlockPos = mobPos.offset(Direction.UP)
-            while (!world.getBlockState(updatedBlockPos).getCollisionShape(world, updatedBlockPos).isEmpty)
-                updatedBlockPos = updatedBlockPos.offset(Direction.UP)
+            else
+            {
+                var updatedBlockPos = mobPos.offset(Direction.UP)
+                while (!world.getBlockState(updatedBlockPos).getCollisionShape(world, updatedBlockPos).isEmpty)
+                    updatedBlockPos = updatedBlockPos.offset(Direction.UP)
 
-            mobPos = updatedBlockPos
-        }
+                mobPos = updatedBlockPos
+            }
 
-        val success = entityType.spawn(world, null, playerIn, mobPos, SpawnReason.MOB_SUMMONED, true, false) != null
+            success == success && entityType.spawn(world, null, playerIn, mobPos, SpawnReason.MOB_SUMMONED, true, false) != null
+        }
 
         return success
     }
